af5d491498ad8f50e8b3b252dfbbe56a2ba0b9ce
“dhrvrm” <dhruvverma028@gmail.com>
2025-01-22 12:33:45 +0530

    Google Analytics with Partytown

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,19 +3,25 @@ import tailwind from '@astrojs/tailwind';
 import mdx from '@astrojs/mdx';
 import react from '@astrojs/react';
 import sitemap from '@astrojs/sitemap';
+import partytown from '@astrojs/partytown';
 
 export default defineConfig({
-  integrations: [
-    tailwind(),
-    mdx(),
-    react(),
-    sitemap()
-  ],
-  markdown: {
-    shikiConfig: {
-      theme: 'dracula',
-      wrap: true
-    }
-  },
-  site: 'https://dhruvverma.dev'
+	integrations: [
+		tailwind(),
+		mdx(),
+		react(),
+		sitemap(),
+		partytown({
+			config: {
+				forward: ['dataLayer.push'],
+			},
+		}),
+	],
+	markdown: {
+		shikiConfig: {
+			theme: 'dracula',
+			wrap: true,
+		},
+	},
+	site: 'https://dhruvverma.dev',
 });
